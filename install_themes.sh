9397bb00b8e33d023ab817bd81e01b35a51c81b1
Reuel R. Ribeiro <rulrok@gmail.com>
2019-02-23 01:59:55 -0300

    style gtk themes

---
+++ install_themes.sh
@@ -0,0 +1,16 @@
+echo "Installing GTK3 theme"
+
+. "$HOME/.themes/install.sh"
+
+echo ""
+echo "----------------------------------------"
+echo "Installing GTK3 icon theme"
+
+. "$HOME/.icons/install.sh"
+. "$HOME/.icons/install_cursor.sh"
+
+echo ""
+echo "----------------------------------------"
+echo ""
+
+lxappearance &
